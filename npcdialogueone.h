363f5b74b5f904d2e4bacd9ef8fd0241319c2156
Syed Muhammad Bin Asif <92625830+syedmba@users.noreply.github.com>
2022-04-03 23:28:26 +0500

    Create npcdialogue.h
    Header file for npcdialogue.cpp

---
+++ npcdialogueone.h
@@ -0,0 +1,12 @@
+// npcdialogueone.h
+
+#ifndef NPCDIALOGUEONE_H
+#define NPCDIALOGUEONE_H
+
+#include <string>
+
+void initiateDialogue(std::string npcName, std::string message);
+void printMessageLine();
+void initiateDialogue(std::string npcName);
+
+#endif
