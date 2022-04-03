3991f7702d8b30789c21f6421da4cc49a530beb5
Syed Muhammad Bin Asif <92625830+syedmba@users.noreply.github.com>
2022-04-03 13:47:28 +0500

    Update printTerrain.cpp
    removed application of ncurses.h library and added comments to explain function and variable usage

--- printterrain.cpp
+++ printterrain.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+// #include <unistd.h>
+
 #include "printterrain.h"
 
 using namespace std;
@@ -7,9 +9,19 @@ using namespace std;
 #define PLAYER  "\xEC\x9B\x83"
 #define FOG     "\xF0\x9F\x8C\xAB"
 
-const double visibilityRadius = 20.0;
+// this bool variable fogOn is a switch to turn on FOG in any area
+// FOG restricts the visibility to a specific radius around the player
+// fogOn may be integrated into printTerrain later to allow for areas with and without fog
 bool fogOn = false;
 
+// the visiblitiy radius when FOG is active
+const double visibilityRadius = 20.0;
+
+
+
+// this function prints the 2D terrain of an area of the game
+// the area is passed as a 2d array called terrain with height and length stored as const values
+// the function also accepts two int values giving the column and row of the player at the current point in time
 void printTerrain(char terrain[mapHeight][mapLength], int playerCol, int playerRow){
 
     for (int i = 0; i < mapHeight; i++){
@@ -32,14 +44,14 @@ void printTerrain(char terrain[mapHeight][mapLength], int playerCol, int playerR
                 cout << FOG;
             }
         }
-        cout << "\r" << endl;
+        cout << endl;
     }
     
     for (int times = 0; times < 1; times++){
         for (int i = 0; i < mapLength; i++){
             cout << "-";
         }
-        cout << "\r" << endl;
+        cout << endl;
     }
     
 
