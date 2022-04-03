8a7e1486c1f3dd0b59a259f477bc480195d161a1
Syed Muhammad Bin Asif <92625830+syedmba@users.noreply.github.com>
2022-04-03 14:09:09 +0500

    Create npcdialogue.cpp
    npcdialogue.cpp will implement interaction with NPCs (non-player characters)

---
+++ npcdialogueone.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+
+// function to carry out the main dialogue
+// uses printMessage()
+void initiateDialogue(){
+    
+}
+
+
+// function to print npc dialogue in a retro-style message box at the bottom of the game window
+void printMessage(){
+
+}
+
+// dummy func
+int main(){
+
+}
