058ca120588be5c4ab5e05dbdcc707f86d4e444e
Syed Muhammad Bin Asif <92625830+syedmba@users.noreply.github.com>
2022-05-07 18:48:54 +0500

    Update battle.h

--- battle.h
+++ battle.h
@@ -8,6 +8,6 @@
 const int totalPlayerSkills = 5;
 const int maxLengthOfSkillName = 20;
 
-void battle(std::string playerActions[totalPlayerSkills][6], double &playerATK, double &playerDEF, double &playerHP, bool &enemyDefeated, std::string playerInventory[inventorySize], std::string playerEquipment[equipmentLimit], double playerActionStats[totalPlayerSkills][6][3]);
+void battle(std::string playerActions[totalPlayerSkills][7], double &playerATK, double &playerDEF, double &playerHP, bool &enemyDefeated, std::string playerInventory[inventorySize], std::string playerEquipment[equipmentLimit], double playerActionStats[totalPlayerSkills][6][3]);
 
 #endif
