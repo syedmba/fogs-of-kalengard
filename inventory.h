4e9d37efe1b915af027821ce46e7dcd04483980c
hossainfahim <fhossain005@gmail.com>
2022-05-07 20:39:50 +0800

    Update inventory.h

--- inventory.h
+++ inventory.h
@@ -12,7 +12,12 @@ std::string Items[ItemsListSize][4] = {
 {"Water", "5", "0", "0"},
 {"Pumpkin seeds", "5", "0", "0"},
 {"Impure Healing Potion", "10", "0", "0"}, 
-{"Regular Healing Potion", "30", "0", "10"},
+{"Regular Healing Potion", "30", "0", "0"},
+{"Beans", "10", "0", "0"},
+{"Soup", "15", "0", "0"},
+{"Touch of Rejuvenation", "40", "0", "0"},
+{"A Second Chance", "70", "0", "0"}
+
 {"Queen Zelda's Healing Drought", "1000", "20", "100"},
 {"Butterfly knife", "1", "5", "0"},
 {"Rusted Shortsword", "5", "10", "0"},
@@ -21,6 +26,7 @@ std::string Items[ItemsListSize][4] = {
 {"Wooden Sword", "5", "15", "5"},
 {"Wooden Axe", "5", "10", "5"},
 {"Shabby Armour", "50", "0", "20"},
+{"Shabby Bow", "5","30", "10"},
 {"Bow of Enchantment", "10", "100", "10"},
 {"Armour of Pikaen Knights", "75", "0", "30"},
 {"Blade of the Pikaen Knights", "75", "75", "0"},
@@ -39,10 +45,11 @@ std::string Items[ItemsListSize][4] = {
 {"Boots of the Ruined King", "100", "100", "50"},
 {"Chestplate of the Ruined King", "500", "0", "1000"},
 {"Excalibur", "400", "400", "0"},
+{""}
 
 };
 
-bool Usable[] = {1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+bool Usable[] = {1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
 
 void SwapItems(std::string playerInventory[inventorySize]);
