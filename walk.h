b74472ed743ab2addabfff0b8e260e6e93d6e869
Syed Muhammad Bin Asif <92625830+syedmba@users.noreply.github.com>
2022-05-07 16:10:08 +0500

    Update walk.h

--- walk.h
+++ walk.h
@@ -6,7 +6,9 @@
 
 #include "battle.h"
 #include "printterrain.h"
+#include "inventory.h"
 
 void walk(char terrain[mapHeight][mapLength], int terrainHeight[mapLength], int monsterPositions[]);
+void giveRandomItem(std::string playerInventory[inventorySize]);
 
 #endif
