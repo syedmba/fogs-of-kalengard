5e9f0d9d17e0440dfa9990ff9219f95f3db0cf93
Syed Muhammad Bin Asif <92625830+syedmba@users.noreply.github.com>
2022-05-07 20:53:16 +0500

    Update skeletonart.h

--- skeletonart.h
+++ skeletonart.h
@@ -7,5 +7,14 @@
 const int artSize = 2800;
 
 void printSkeleton();
+void printGoblin();
+void printDragon();
+void printDemon();
+void printOgre();
+void printWolf();
+void printDarkElf();
+void printElemental();
+void printWyvern();
+void printDryad();
 
 #endif
